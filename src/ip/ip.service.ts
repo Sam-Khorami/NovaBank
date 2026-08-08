3af1543ee8a41e8870e1a5840ad0102bed3e4bd0
Sam <samkhorrami84@gmail.com>
2026-08-08 16:24:51 +0330

    Repository Injected In Ip Service

--- src/ip/ip.service.ts
+++ src/ip/ip.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Ip } from '../entity/ip.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class IpService {}
+export class IpService {
+
+    constructor (
+
+        @InjectRepository(Ip) private readonly ipRepo: Repository<Ip>
+
+    ) {}
+
+}
