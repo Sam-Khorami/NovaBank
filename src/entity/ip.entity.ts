b669856712141f52d8fffb94e043f584590fc4d7
Sam <samkhorrami84@gmail.com>
2026-08-08 16:19:02 +0330

    Ip Entity Added

---
+++ src/entity/ip.entity.ts
@@ -0,0 +1,31 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity("ip")
+export class Ip {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ type: "varchar", nullable: false })
+    ip!: string;
+
+    @Column({ type: "int", nullable: false, default: 0 })
+    requestCount!: number;
+
+    @Column({ type: "timestamp", nullable: false })
+    windowStart!: Date;
+
+    @Column({ type: "boolean", nullable: false, default: false })
+    isBlocked!: boolean;
+
+    @Column({ type: "timestamp", nullable: true })
+    blockedUntil!: Date | null;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date;
+
+}
