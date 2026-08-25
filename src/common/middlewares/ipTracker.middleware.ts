34236219669463e9f18804a326e8b6edaa280b09
Sam <samkhorrami84@gmail.com>
2026-08-25 23:04:26 +0330

    Adding Some Changes

--- src/common/middlewares/ipTracker.middleware.ts
+++ src/common/middlewares/ipTracker.middleware.ts
@@ -1,6 +1,7 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from "express";
-import { IpService } from '../ip/ip.service';
+import { IpService } from 'src/modules/ip/ip.service';
+
 
 @Injectable()
 export class IpTrackerMiddleware implements NestMiddleware {
