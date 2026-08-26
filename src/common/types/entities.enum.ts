5caeccb2a8eb34c9bb352fe581fef06e4d39586f
Sam <samkhorrami84@gmail.com>
2026-08-27 01:15:17 +0330

    Wallet Status Enum Added

--- src/common/types/entities.enum.ts
+++ src/common/types/entities.enum.ts
@@ -1,7 +1,11 @@
 export enum UserRoleEnum {
-
     USER = "user",
     ADMIN = "admin",
     SUPERADMIN = "superAdmin"
+}
 
+export enum WalletStatusEnum {
+    Active = "active",
+    Closed = "closed",
+    Blocked = "blocked"
 }
