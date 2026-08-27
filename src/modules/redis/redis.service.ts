f805620c3aab3df2e4e4b8834f2f8c58553f486a
Sam <samkhorrami84@gmail.com>
2026-08-27 19:03:08 +0330

    Redis Service Is Ready

--- src/modules/redis/redis.service.ts
+++ src/modules/redis/redis.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class RedisService {}
+export class RedisService {
+
+    constructor (
+
+        @Inject(CACHE_MANAGER) private cacheManager: Cache
+
+    ) {}
+
+    async setOtp (key: string, value: string, ttl?: number) {
+
+        return await this.cacheManager.set(key, value, ttl);
+
+    }
+
+    async get (key: string) {
+
+        return await this.cacheManager.get(key);
+
+    }
+
+    async delete (key: string) {
+
+        return await this.cacheManager.del(key);
+
+    }
+
+}
