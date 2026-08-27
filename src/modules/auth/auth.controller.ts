2195449cdae77b0d8199eb4dad7f0e483df8c415
Sam <samkhorrami84@gmail.com>
2026-08-27 19:24:34 +0330

    Sign Up Service Added

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
+import { SignUpDto } from './dto/signup.dto';
 
+@ApiTags("Auth Management")
 @Controller('auth')
 export class AuthController {
+
   constructor(private readonly authService: AuthService) {}
+
+
+  @Post("signup")
+  async signUp (@Body() data: SignUpDto) {
+
+    return await this.authService.signUp(data);
+
+  }
+
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entity/users.entity';
+import { Repository } from 'typeorm';
+import { SignUpDto } from './dto/signup.dto';
+import { VitalRecordsService } from '../vital-records/vital-records.service';
+import { MailService } from '../mail/mail.service';
+import { RedisService } from '../redis/redis.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor (
+
+        @InjectRepository(User) private readonly userRepo: Repository<User>,
+        private readonly vitalRecordsService: VitalRecordsService,
+        private readonly redisService: RedisService,
+        private readonly mailService: MailService
+
+    ) {}
+
+    async signUp (data: SignUpDto) {
+
+        const checkUser = await this.userRepo.findOne({ where: { phoneNumber: data.phoneNumber } });
+        if (checkUser) throw new ConflictException("User already signed up");
+
+        await this.vitalRecordsService.isMatch(data.phoneNumber, data.nationalCode);
+        const otpCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+        const newUnverifiedUser = this.userRepo.create({ phoneNumber: data.phoneNumber, email: data.email, nationalCode: data.nationalCode, password: data.password });
+        await this.userRepo.save(newUnverifiedUser);
+        
+        await this.redisService.setOtp(`otp:${data.phoneNumber}`, otpCode, 120000)
+        await this.mailService.sendOtp(data.email, otpCode);
+
+        return { message: "The otp Code is sent to you" }
+
+    }
+
+}
