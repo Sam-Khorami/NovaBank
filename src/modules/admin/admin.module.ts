b881bf74261069fd2902ae75f40fec90ae8c4e5f
Sam <samkhorrami84@gmail.com>
2026-08-28 11:51:18 +0330

    Entities Are Using In Admin Module

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entity/users.entity';
+import { Role } from 'src/entity/role.entity';
+import { Permission } from 'src/entity/permission.entity';
 
 @Module({
+  imports: [
+
+    TypeOrmModule.forFeature([User, Role, Permission])
+
+  ],
   controllers: [AdminController],
   providers: [AdminService],
 })
