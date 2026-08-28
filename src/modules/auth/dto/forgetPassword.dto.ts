e819f9dc1f7c523f80bb8fad982300cdab7522c1
Sam <samkhorrami84@gmail.com>
2026-08-28 19:34:48 +0330

    Forget Password Dto Added

---
+++ src/modules/auth/dto/forgetPassword.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, Matches } from "class-validator";
+
+
+export class ForgetPasswordDto {
+
+    @ApiProperty({ example: "09025244094", description: "Enter the phoneNumber field" })
+    @IsNotEmpty({ message: "The phoneNumber field can not be empty" })
+    @IsString({ message: "The phoneNumber field must be a string" })
+    @Matches(/^09\d{9}$/, { message: "You phone number structure must be like this: 09025244094"})
+    phoneNumber: string;
+
+}
