bcaff6aaba6b31b9f8794266c50ca5dafc5a72e3
Sam <samkhorrami84@gmail.com>
2026-08-31 01:07:12 +0330

    The Base Swagger For Admin Controller Is Ready

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwtAuth.guard';
 
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+@ApiTags("Admin Management")
 @Controller('admin')
 export class AdminController {
+
   constructor(private readonly adminService: AdminService) {}
+
 }
