719e6a37af949420336f9eb75a598235db0d3666
Sam <samkhorrami84@gmail.com>
2026-08-31 01:08:03 +0330

    Get User Permissions Added

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entity/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+
+    constructor (
+
+        @InjectRepository(User) private readonly userRepo: Repository<User>
+
+    ) {}
+
+    async getPermissions (userId: string) {
+
+        const user = await this.userRepo.findOne({ where: { id: userId }, relations: { permissions: true, roles: { permissions: true } } });
+        if (!user) throw new NotFoundException("User Not Found!");
+
+        const permissions = new Set<string>();
+
+        user.permissions.forEach((item) => {
+
+            permissions.add(item.name);
+
+        });
+
+        user.roles.forEach((item) => {
+
+            item.permissions.forEach((permission) => {
+
+                permissions.add(permission.name);
+
+            })
+
+        });
+
+        return Array.from(permissions);
+
+    }
+
+}
