18fb2ab40ebbd5fdf593ce71ac7376fa82bb4114
Sam <samkhorrami84@gmail.com>
2026-09-13 10:38:59 +0330

    Adding New Permission In Enum

--- src/common/types/permissions.enum.ts
+++ src/common/types/permissions.enum.ts
@@ -1,3 +1,4 @@
 export enum PermissionsEnum {
-    ADMIN_ACCESS_ADD_PERMISSION = "admin:permission:add"
+    ADMIN_ACCESS_ADD_PERMISSION = "admin:permission:add",
+    ADMIN_ACCESS_ADD_ROLE = "admin:role:add"
 }
