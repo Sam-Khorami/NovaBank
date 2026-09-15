abb0407c7051a699d7159b36dcd2ba37220880fa
Sam <samkhorrami84@gmail.com>
2026-09-15 15:16:12 +0330

    Some New Permission Added

--- src/common/types/permissions.enum.ts
+++ src/common/types/permissions.enum.ts
@@ -1,5 +1,9 @@
 export enum PermissionsEnum {
     ADMIN_ACCESS_ADD_PERMISSION = "admin:permission:add",
+    ADMIN_ACCESS_DELETE_PERMISSION = "admin:permission:delete",
+    ADMIN_ACCESS_READ_PERMISSION = "admin:permission:read",
     ADMIN_ACCESS_ADD_ROLE = "admin:role:add",
-    ADMIN_ACCESS_GET_USERS = "admin:user:read"
+    ADMIN_ACCESS_GET_USERS = "admin:user:read",
+    ADMIN_ACCESS_DELETE_ROLE = "admin:role:delete",
+    ADMIN_ACCESS_READ_ROLE = "admin:role:read",
 }
