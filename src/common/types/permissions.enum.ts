0b0c2634018a4ad73d800c7ba425ccf23a6b000c
Sam <samkhorrami84@gmail.com>
2026-09-17 16:16:27 +0330

    Adding New Permission Enum

--- src/common/types/permissions.enum.ts
+++ src/common/types/permissions.enum.ts
@@ -1,5 +1,6 @@
 export enum PermissionsEnum {
     ADMIN_ACCESS_ASSIGN_ROLE = "admin:role:assign",
+    ADMIN_ACCESS_ASSIGN_PERMISSION = "admin:permission:assign",
     ADMIN_ACCESS_ADD_PERMISSION = "admin:permission:add",
     ADMIN_ACCESS_DELETE_PERMISSION = "admin:permission:delete",
     ADMIN_ACCESS_READ_PERMISSION = "admin:permission:read",
