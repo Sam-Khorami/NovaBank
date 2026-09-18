9abad2bbbb4baea2fecc1de19dad4a9cfbdc6348
Sam <samkhorrami84@gmail.com>
2026-09-18 13:10:54 +0330

    Setup For Users Service Added

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entity/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor (
+
+        @InjectRepository(User) private readonly userRepo: Repository<User>
+
+    ) {}
+
+}
