f4cb7f3427a9e8989086e8a66ee48ac13bce9bb2
Sam <samkhorrami84@gmail.com>
2026-09-18 13:10:35 +0330

    Users Controller Added

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwtAuth.guard';
+import { PermissionGuard } from 'src/common/guards/permission.guard';
 
+@ApiTags("Users Managment")
+@UseGuards(JwtAuthGuard, PermissionGuard)
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
