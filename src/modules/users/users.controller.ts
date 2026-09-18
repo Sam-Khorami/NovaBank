26d3f8edb96674d63ac0c8f4401ceb2c30d419b4
Sam <samkhorrami84@gmail.com>
2026-09-18 15:33:18 +0330

    Upload Document Service Added

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,83 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { BadRequestException, Controller, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/common/guards/jwtAuth.guard';
 import { PermissionGuard } from 'src/common/guards/permission.guard';
+import { existsSync, mkdirSync } from 'fs';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from "multer";
+import { extname } from "path";
 
 @ApiTags("Users Managment")
 @UseGuards(JwtAuthGuard, PermissionGuard)
 @ApiBearerAuth()
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  
+  constructor(private readonly usersService: UsersService) {
+
+    if (!existsSync("./uploads")) {
+
+      mkdirSync("./uploads", { recursive: true })
+
+    }
+
+  }
+
+
+  @ApiOperation({ summary: "Upload Document For Review", description: "With this api user can upload its document and await for answer from admin" })
+  @ApiConsumes("multipart/form-data")
+  @ApiBody({
+
+    schema: {
+
+      type: "object",
+      properties: {
+
+        image: {
+
+          type: "string",
+          format: "binary"
+
+        }
+
+      }
+
+    }
+
+  })
+  @UseInterceptors(
+
+    FileInterceptor(
+
+      "image", {
+
+        storage: diskStorage({ destination: "./uploads", filename(req, file, cb) {
+
+          const uniqueFileName = `${Date.now()}-${Math.round(Math.random() * 1000)}${extname(file.originalname)}`;
+          cb(null, uniqueFileName);
+
+        } }),
+        limits: { fileSize: 5 * 1024 * 1024 },
+        fileFilter: (req, file, cb) => {
+
+          const allowedMimeTypes = ["image/png", "image/jpg", "image/jpeg", "image/webp"];
+          if (allowedMimeTypes.includes(file.mimetype)) cb(null, true);
+          else cb(new BadRequestException("Bad File Format"), false);
+
+        }
+
+      }
+
+    )
+
+  )
+  @Post("upload-document")
+  async uploadDocument (@UploadedFile() image: Express.Multer.File, @Req() request: Request) {
+
+    if (!image) throw new BadRequestException("Image didn't uploaded successfully!");
+    return await this.usersService.uploadDocument(image, request);
+
+  }
+
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { KycStatusEnum } from 'src/common/types/entities.enum';
+import { Documents } from 'src/entity/documents.entity';
 import { User } from 'src/entity/users.entity';
 import { Repository } from 'typeorm';
 
@@ -8,8 +10,29 @@ export class UsersService {
 
     constructor (
 
-        @InjectRepository(User) private readonly userRepo: Repository<User>
+        @InjectRepository(User) private readonly userRepo: Repository<User>,
+        @InjectRepository(Documents) private readonly documentsRepo: Repository<Documents>
 
     ) {}
+
+    async uploadDocument (image: Express.Multer.File, request: Request) {
+
+        const userId = request["user"].id;
+        const user = await this.userRepo.findOne({ where: { id: userId } });
+        if (!user) throw new NotFoundException("The user not found");
+        if (user.kycStatus !== KycStatusEnum.NOT_SUBMITTED) throw new BadRequestException("Your request set already");
+
+        const checkDocument = await this.documentsRepo.findOne({ where: { userId } });
+        if (checkDocument) throw new ConflictException("Your request set already");
+
+        const documentName = `/uploads/${image.filename}`;
+        const newDocument = this.documentsRepo.create({ file: documentName, user: { id: userId }, userId });
+        user.kycStatus = KycStatusEnum.UNDER_REVIEW;
+
+        await this.documentsRepo.save(newDocument);
+        await this.userRepo.save(user);
+        return { message: "Your document uploaded and your request set" }
+
+    }
 
 }
