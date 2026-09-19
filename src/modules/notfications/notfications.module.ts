b4716484bca107c0cf22d7981f68af029e5389b3
Sam <samkhorrami84@gmail.com>
2026-09-19 19:17:45 +0330

    Entities Added In Notfications Module

--- src/modules/notfications/notfications.module.ts
+++ src/modules/notfications/notfications.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { NotficationsService } from './notfications.service';
 import { NotficationsController } from './notfications.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entity/users.entity';
+import { Notfications } from 'src/entity/notfication.entity';
 
 @Module({
+  imports: [
+
+    TypeOrmModule.forFeature([User, Notfications])
+
+  ],
   controllers: [NotficationsController],
   providers: [NotficationsService],
 })
