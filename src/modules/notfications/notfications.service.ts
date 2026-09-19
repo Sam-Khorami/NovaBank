535d7648ebabd3edc85124a61189418416cb29e8
Sam <samkhorrami84@gmail.com>
2026-09-19 19:18:43 +0330

    Repositories Injected In Notfication Service

--- src/modules/notfications/notfications.service.ts
+++ src/modules/notfications/notfications.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Notfications } from 'src/entity/notfication.entity';
+import { User } from 'src/entity/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class NotficationsService {}
+export class NotficationsService {
+
+    constructor (
+
+        @InjectRepository(User) private readonly userRepo: Repository<User>,
+        @InjectRepository(Notfications) private readonly notficationRepo: Repository<Notfications>
+
+    ) {}
+
+}
