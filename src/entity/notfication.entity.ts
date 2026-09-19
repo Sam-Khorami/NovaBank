90028c8afbcf2df8a005ae7c92144439a4403ee7
Sam <samkhorrami84@gmail.com>
2026-09-19 19:00:19 +0330

    Notfication Entity Added

---
+++ src/entity/notfication.entity.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, Index, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { User } from "./users.entity";
+
+
+export class Notfications {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({ type: "varchar", nullable: false })
+    title: string;
+
+    @Column({ type: "text", nullable: false })
+    message: string;
+
+    @Column({ type: "boolean", nullable: false, default: false })
+    isRead: boolean;
+
+    @ManyToOne(() => User, (user) => user.notfications)
+    user: User;
+
+    @Index()
+    @Column({ type: "uuid" })
+    userId: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+}
