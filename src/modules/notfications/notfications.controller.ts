2bc46f8b518e0aea403e6758b4216a3532ea8982
Sam <samkhorrami84@gmail.com>
2026-09-19 19:18:16 +0330

    Structure For Controller Is Ready

--- src/modules/notfications/notfications.controller.ts
+++ src/modules/notfications/notfications.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { NotficationsService } from './notfications.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwtAuth.guard';
 
+@ApiTags("Notfications Management")
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('notfications')
 export class NotficationsController {
+
   constructor(private readonly notficationsService: NotficationsService) {}
+
 }
