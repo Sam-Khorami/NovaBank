5fb46706f9303e6bdc1db2707b69b2f8fc728865
Sam <samkhorrami84@gmail.com>
2026-09-20 11:47:47 +0330

    Entities Added In User Module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -6,10 +6,12 @@ import { User } from 'src/entity/users.entity';
 import { Documents } from 'src/entity/documents.entity';
 import { AdminModule } from '../admin/admin.module';
 import { NotficationsModule } from '../notfications/notfications.module';
+import { Wallet } from 'src/entity/wallet.entity';
+import { WalletTransaction } from 'src/entity/walletTransaction.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Documents]),
+    TypeOrmModule.forFeature([User, Documents, Wallet, WalletTransaction]),
     AdminModule,
     NotficationsModule
   ],
