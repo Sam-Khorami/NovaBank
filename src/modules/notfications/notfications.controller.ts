1935b529a6518b13e274f749818e87d0c084c135
Sam <samkhorrami84@gmail.com>
2026-09-20 10:43:52 +0330

    Get Unread Notfications Controller Addded

--- src/modules/notfications/notfications.controller.ts
+++ src/modules/notfications/notfications.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { NotficationsService } from './notfications.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/common/guards/jwtAuth.guard';
 
 @ApiTags("Notfications Management")
@@ -10,5 +10,13 @@ import { JwtAuthGuard } from 'src/common/guards/jwtAuth.guard';
 export class NotficationsController {
 
   constructor(private readonly notficationsService: NotficationsService) {}
+
+  @ApiOperation({ summary: "Get Unread Notfications", description: "With this api user could see all unread notfications" })
+  @Get("unreads")
+  async getUnreadNotfications (@Req() request: Request) {
+
+    return await this.notficationsService.getUnreadNotfications(request);
+
+  }
 
 }
