72bd99ea5258c84953130243d0e7cf16c03f0f9f
Sam <samkhorrami84@gmail.com>
2026-09-21 12:21:01 +0330

    Repositories Injected In Wallet Service

--- src/modules/wallet/wallet.service.ts
+++ src/modules/wallet/wallet.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Idempotency } from 'src/entity/Idempotency.entity';
+import { Transfers } from 'src/entity/transfers.entity';
+import { User } from 'src/entity/users.entity';
+import { Wallet } from 'src/entity/wallet.entity';
+import { WalletTransaction } from 'src/entity/walletTransaction.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class WalletService {}
+export class WalletService {
+
+    constructor (
+
+        @InjectRepository(User) private readonly userRepo: Repository<User>,
+        @InjectRepository(Wallet) private readonly walletRepo: Repository<Wallet>,
+        @InjectRepository(WalletTransaction) private readonly transactionRepo: Repository<WalletTransaction>,
+        @InjectRepository(Idempotency) private readonly idempotencyRepo: Repository<Idempotency>,
+        @InjectRepository(Transfers) private readonly transfersRepo: Repository<Transfers>
+
+    ) {}
+
+}
