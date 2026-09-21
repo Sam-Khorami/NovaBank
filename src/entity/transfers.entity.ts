7e56292aef1237f9764e84353ce04cf712f0d34b
Sam <samkhorrami84@gmail.com>
2026-09-21 11:25:31 +0330

    Amount Field Added In Transfers Entity

--- src/entity/transfers.entity.ts
+++ src/entity/transfers.entity.ts
@@ -8,6 +8,9 @@ export class Transfers {
     @PrimaryGeneratedColumn("uuid")
     id: string;
 
+    @Column({ type: 'numeric', precision: 20, scale: 8, nullable: false })
+    amount: number;
+
     @ManyToOne(() => User, (user) => user.senders, { onDelete: "CASCADE" })
     sender: User;
 
