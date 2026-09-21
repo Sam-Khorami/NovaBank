27c1702f1e04261c7ae25abfadc7f2adfdf337bb
Sam <samkhorrami84@gmail.com>
2026-09-21 12:20:23 +0330

    Structure For Wallet Controller Added

--- src/modules/wallet/wallet.controller.ts
+++ src/modules/wallet/wallet.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { WalletService } from './wallet.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwtAuth.guard';
+import { KycGuard } from 'src/common/guards/kyc.guard';
+import { KycOnly } from 'src/common/decorators/kyc.decorator';
 
+@ApiTags("Wallet Management")
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, KycGuard)
+@KycOnly()
 @Controller('wallet')
 export class WalletController {
+  
   constructor(private readonly walletService: WalletService) {}
+
 }
