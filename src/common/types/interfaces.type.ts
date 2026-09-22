d9444408796fe1cf0b73a89a5005ef7e5e51038c
Sam <samkhorrami84@gmail.com>
2026-09-22 20:29:42 +0330

    New Interface Added

--- src/common/types/interfaces.type.ts
+++ src/common/types/interfaces.type.ts
@@ -1,8 +1,13 @@
 import { UserRoleEnum } from "./entities.enum";
 
 export interface Payload {
-
     id: string,
     role: UserRoleEnum
+}
 
+export interface TransferNotficationData {
+    userId: string,
+    title: string,
+    message: string,
+    email: string
 }
