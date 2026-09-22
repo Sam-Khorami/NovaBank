dd0ba81fd12d92394b3872ca7aeb56d2207a9a36
Sam <samkhorrami84@gmail.com>
2026-09-22 20:30:55 +0330

    Bull Module Added In Notfications Module

--- src/modules/notfications/notfications.module.ts
+++ src/modules/notfications/notfications.module.ts
@@ -4,15 +4,20 @@ import { NotficationsController } from './notfications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entity/users.entity';
 import { Notfications } from 'src/entity/notfication.entity';
+import { BullModule } from '@nestjs/bull';
+import { MailModule } from '../mail/mail.module';
+import { NotficationProcessor } from './notfication.processor';
 
 @Module({
   imports: [
 
-    TypeOrmModule.forFeature([User, Notfications])
+    TypeOrmModule.forFeature([User, Notfications]),
+    BullModule.registerQueue({ name: "notfications" }),
+    MailModule
 
   ],
   controllers: [NotficationsController],
-  providers: [NotficationsService],
+  providers: [NotficationsService, NotficationProcessor],
   exports: [NotficationsService]
 })
 export class NotficationsModule {}
