e810fa18d26cc70e9fe382b0f587d6d38f8cee05
Sam <samkhorrami84@gmail.com>
2026-09-23 18:38:30 +0330

    Virtual Card Controller Structure Added

--- src/modules/virtual_card/virtual_card.controller.ts
+++ src/modules/virtual_card/virtual_card.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { VirtualCardService } from './virtual_card.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwtAuth.guard';
+import { KycGuard } from 'src/common/guards/kyc.guard';
 
+@ApiTags("Virtual Card Management")
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, KycGuard)
 @Controller('virtual-card')
 export class VirtualCardController {
+
   constructor(private readonly virtualCardService: VirtualCardService) {}
+
 }
