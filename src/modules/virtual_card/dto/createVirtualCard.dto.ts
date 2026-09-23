54c33fc6d5773f608b06c3d8dc9ccec6e0d4aa69
Sam <samkhorrami84@gmail.com>
2026-09-24 00:20:52 +0330

    Create Virtual Card Dto Added

---
+++ src/modules/virtual_card/dto/createVirtualCard.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsEnum, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, Max, Min } from "class-validator";
+import { VirtualCardType } from "src/common/types/entities.enum";
+
+
+export class CreateVirtualCardDto {
+
+    @ApiPropertyOptional({ example: "test", description: "Enter the lable field" })
+    @IsString({ message: "The lable field can not be empty" })
+    @IsOptional()
+    lable: string;
+
+    @ApiProperty({ example: 500000, description: "Enter the spending limit field" })
+    @IsNotEmpty({ message: "The spending limit field can not be empty" })
+    @IsNumber({}, { message: "The spending limit field must be a number" })
+    @Min(1000, { message: "The spending limit can not be less than 1000 toman" })
+    spendingLimit: number;
+
+    @ApiProperty({ example: 12, description: "Enter the expiryMonth field" })
+    @IsNotEmpty({ message: "The expiry month field can not be empty" })
+    @IsInt({ message: "The expiry month field must be a number" })
+    @Min(1, { message: "The expiry month can not be less than 1 month" })
+    @Max(24, { message: "The expiry month can not be greater than 24 month" })
+    expiryMonth: number;
+
+    @ApiProperty({ enum: VirtualCardType, enumName: "VirtualCardType", example: VirtualCardType.STANDARD, description: "Enter the cardType field" })
+    @IsNotEmpty({ message: "The card type field can not be empty" })
+    @IsEnum(VirtualCardType)
+    cardType: VirtualCardType;
+
+}
