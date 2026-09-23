1c84724baac506913cef129d10a04d7eb815ef8a
Sam <samkhorrami84@gmail.com>
2026-09-24 00:21:37 +0330

    Check Luhn Algorithm Service Added

--- src/modules/virtual_card/virtual_card.service.ts
+++ src/modules/virtual_card/virtual_card.service.ts
@@ -18,4 +18,38 @@ export class VirtualCardService {
 
     ) {}
 
+    async checkLuhnAlgorithm (cardNumber: string) {
+
+        let index = 14;
+        let lastResult = 0;
+
+        while (index !== -2) {
+
+            let multipleTwo = Number(cardNumber[index]) * 2;
+            let mainNumber = 0;
+
+            if (multipleTwo > 9) mainNumber = multipleTwo - 9;
+            else mainNumber = multipleTwo;
+
+            let array = cardNumber.split("");
+            array[index] = String(mainNumber);
+            cardNumber = array.join("");
+
+            index -= 2;
+
+        }
+
+        let array = cardNumber.split("");
+
+        for (let char of array) {
+
+            lastResult += Number(char);
+
+        }
+
+        if (lastResult % 10 === 0) return true;
+        else return false;
+
+    }
+
 }
