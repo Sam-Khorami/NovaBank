31ef046441584d7ef3019ad6cb446fa326847420
Sam <samkhorrami84@gmail.com>
2026-09-23 18:41:44 +0330

    Repositories Injected In Virtual Card Service

--- src/modules/virtual_card/virtual_card.service.ts
+++ src/modules/virtual_card/virtual_card.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entity/users.entity';
+import { VirtualCard } from 'src/entity/virtualCard.entity';
+import { VirtualCardTransaction } from 'src/entity/virtualCardTransaction.entity';
+import { Wallet } from 'src/entity/wallet.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class VirtualCardService {}
+export class VirtualCardService {
+
+    constructor (
+
+        @InjectRepository(User) private readonly userRepo: Repository<User>,
+        @InjectRepository(Wallet) private readonly walletRepo: Repository<Wallet>,
+        @InjectRepository(VirtualCard) private readonly virtualCardRepo: Repository<VirtualCard>,
+        @InjectRepository(VirtualCardTransaction) private readonly virtualCardTransactionRepo: Repository<VirtualCardTransaction>
+
+    ) {}
+
+}
