367f4c9cc822fd1c131bd4fab06a1dc21917ad21
Sam <samkhorrami84@gmail.com>
2026-09-24 12:20:45 +0330

    Create Virtual Card Service Added

--- src/modules/virtual_card/virtual_card.service.ts
+++ src/modules/virtual_card/virtual_card.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entity/users.entity';
 import { VirtualCard } from 'src/entity/virtualCard.entity';
 import { VirtualCardTransaction } from 'src/entity/virtualCardTransaction.entity';
 import { Wallet } from 'src/entity/wallet.entity';
 import { Repository } from 'typeorm';
+import { CreateVirtualCardDto } from './dto/createVirtualCard.dto';
+import { CardTypeQueryDto } from './dto/cardTypeQuery.dto';
+import { Decimal } from 'decimal.js';
 
 @Injectable()
 export class VirtualCardService {
@@ -18,7 +21,7 @@ export class VirtualCardService {
 
     ) {}
 
-    async checkLuhnAlgorithm (cardNumber: string) {
+    private checkLuhnAlgorithm (cardNumber: string) {
 
         let index = 14;
         let lastResult = 0;
@@ -49,6 +52,51 @@ export class VirtualCardService {
 
         if (lastResult % 10 === 0) return true;
         else return false;
+
+    }
+
+    async createVirtualCard (data: CreateVirtualCardDto, query: CardTypeQueryDto, request: Request) {
+
+        const userId = request["user"].id;
+        const user = await this.userRepo.findOne({ where: { id: userId }, relations: { wallet: true } });
+
+        let checkLuhn = false;
+        let mainCardNumber: string;
+
+        while (!checkLuhn) {
+
+            const randomCardNumber = Math.floor(1000000000 + Math.random() * 9000000000).toString();
+            let cardNumber = `603799${randomCardNumber}`;
+
+            const checkCardNumber = this.checkLuhnAlgorithm(cardNumber);
+            if (checkCardNumber) {
+
+                const checkVirtualCard = await this.virtualCardRepo.findOne({ where: { cardNumber } });
+                const checkWallet = await this.walletRepo.findOne({ where: { cardNumber } });
+                
+                if (!checkVirtualCard && !checkWallet) {
+
+                    mainCardNumber = cardNumber;
+                    checkLuhn = true;
+
+                }
+
+            }
+
+        }
+        
+        const last4Digits = mainCardNumber.slice(12, 16);
+        const cvv2 = Math.floor(100 + Math.random() * 900).toString();
+
+        const now = new Date()
+        const expiryDate = new Date(now);
+        expiryDate.setMonth(expiryDate.getMonth() + data.expiryMonth);
+
+        const spendingLimit = new Decimal(data.spendingLimit);
+        const newVirtualCard = this.virtualCardRepo.create({ lable: data.lable, cardNumber: mainCardNumber, last4Digits, cvv2, cardType: query.cardType, spendingLimit: spendingLimit.toFixed(8), wallet: { id: user.wallet.id }, walletId: user.wallet.id, expiryDate });
+        await this.virtualCardRepo.save(newVirtualCard);
+
+        return { message: "Your virtual card was successfully created", info: { mainCardNumber, cvv2, spendingLimit, expiryDate } }
 
     }
 
