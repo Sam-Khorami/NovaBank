f1ba1fce41bfdb8aa4a61d046b8f213d215cab05
Sam <samkhorrami84@gmail.com>
2026-09-26 14:08:22 +0330

    Some New Module Added

--- src/modules/wallet/wallet.module.ts
+++ src/modules/wallet/wallet.module.ts
@@ -10,13 +10,15 @@ import { Transfers } from 'src/entity/transfers.entity';
 import { NotficationsModule } from '../notfications/notfications.module';
 import { MailModule } from '../mail/mail.module';
 import { BullModule } from '@nestjs/bull';
+import { VirtualCardModule } from '../virtual_card/virtual_card.module';
 
 @Module({
   imports: [
 
     TypeOrmModule.forFeature([User, Wallet, WalletTransaction, Idempotency, Transfers]),        
     NotficationsModule,
-    MailModule
+    MailModule,
+    VirtualCardModule
 
   ],
   controllers: [WalletController],
